d1fd9a37386c85cf0efa4cb05e93614b234e64b0
IsaiahFlax <flax.isaiah@gmail.com>
2020-01-31 19:24:58 -0700

    starting with importing components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from "./components/PostsContainer/PostsPage"
+import SearchBar from "./components/SearchBar"
+
 // import dummydata
+import "./dummy-data"
 
 const App = () => {
+  const[userInfo, setUserInfo]
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
